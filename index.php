0bb351137abbbfcda165ba5ca466442b8212d99f
kijimaD <norimaking777@gmail.com>
2022-12-11 23:57:13 +0900

    feat: initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>check</title>
+</head>
+<body>
+    <h1>
+        <?php print ("PHPのprint関数によって出力された"); ?>
+    </h1>
+</body>
+</html>
